5da46324584985e2d5439e47400d65a361fafa54
FoxKito <865791187@qq.com>
2020-10-24 15:53:16 +0800

    idea的提交2

--- springboot01/src/main/java/com/atguigu/springboot/controller/HelloController.java
+++ springboot01/src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -2,7 +2,7 @@ package com.atguigu.springboot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/////////
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
